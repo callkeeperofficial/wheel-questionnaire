8e8095b571fe3888f8ddacb21948a3e253dac491
Jim Raynor <lostlose@yandex.ru>
2023-07-13 18:04:55 +0700

    Поменял ссылку на приложение

--- components/utils/StaticFunctions.tsx
+++ components/utils/StaticFunctions.tsx
@@ -1,7 +1,7 @@
 import { Router } from "../../types/RouterType";
 
 export const goToLiferadar = (deepLinkValue: string) => () =>
-    window.open("https://liferadar.onelink.me/IwFG/5x9vfml6?deep_link_value=" + deepLinkValue, "blank");
+    window.open("https://liferadar.onelink.me/IwFG/n7yuoky4?deep_link_value=" + deepLinkValue, "blank");
 
 export const openPrivacy = () => window.open("https://liferadar.app/privacy", "blank");
 
