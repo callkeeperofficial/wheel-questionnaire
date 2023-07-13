af2b2c42bc86d315b8ed29361af524b08b70418b
Jim Raynor <lostlose@yandex.ru>
2023-07-13 17:45:33 +0700

    Поставил локализацию по умолчанию

--- components/molecules/i18n.tsx
+++ components/molecules/i18n.tsx
@@ -5,12 +5,15 @@ import wheelQuestions from "../../assets/data/wheel-questions.json";
 
 console.log("i18n.js", getLocales()[0].languageCode);
 
+const knownLocale = Object.keys(translation).find(t => t === getLocales()[0].languageCode);
+const locale = knownLocale || "en";
+
 export const i18n = new I18n({...translation});
-i18n.locale = getLocales()[0].languageCode;
+i18n.locale = locale;
 // i18n.locale = "ru";
 
 export const i18nQuestions = new I18n({...wheelQuestions});
-i18nQuestions.locale = getLocales()[0].languageCode;
+i18nQuestions.locale = locale;
 // i18nQuestions.locale = "ru";
 
 export const changeLanguage = (locale: string | undefined) => {
