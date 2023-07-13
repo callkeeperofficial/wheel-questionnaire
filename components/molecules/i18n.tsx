6321bf35640e1ec2999d86142d398ab16097e8fc
Jim Raynor <lostlose@yandex.ru>
2023-07-13 17:28:02 +0700

    Убрал язык

--- components/molecules/i18n.tsx
+++ components/molecules/i18n.tsx
@@ -3,6 +3,8 @@ import translation from "../../assets/data/i18n.json";
 import {getLocales} from "expo-localization";
 import wheelQuestions from "../../assets/data/wheel-questions.json";
 
+console.log("i18n.js", getLocales()[0].languageCode);
+
 export const i18n = new I18n({...translation});
 i18n.locale = getLocales()[0].languageCode;
 // i18n.locale = "ru";
